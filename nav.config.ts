89162bc3ce9b7fd7c57e6bbe678c5c8edc63e7b2
yephm <49516742+yephm@users.noreply.github.com>
2021-03-31 20:25:10 +0800

    Update nav.config.ts

--- nav.config.ts
+++ nav.config.ts
@@ -2,7 +2,7 @@ import { IConfig } from './src/types'
 
 const c: IConfig = {
   // [必填], 请填写您的仓库地址
-  gitRepoUrl: 'https://github.com/xjh22222228/nav',
+  gitRepoUrl: 'https://github.com/yephm/nav',
 
   // 部署分支
   branch: 'main',
@@ -11,16 +11,16 @@ const c: IConfig = {
   hashMode: true,
 
   // 是否显示右上角的 Github 图标
-  showGithub: true,
+  showGithub: false,
 
   // 您的网站地址，有利于SEO
-  homeUrl: 'https://nav3.cn',
+  homeUrl: 'https://yephm.github.io/nav',
 
   // 网站标题
-  title: '发现导航 - 精选实用导航网站',
+  title: '学习Plus - 精选实用导航网站',
 
   // 网站描述
-  description: '发现导航 - 精选实用导航网站',
+  description: '学习Plus - 精选实用导航网站',
 
   // 网站关键字
   keywords: '导航,前端资源,社区站点,设计师,实用工具,学习资源,运营,网络安全,node.js',
